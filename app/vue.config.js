a573d4474c08b89e41982912091e34e2fb6c41ee
Ruud Boon <ruud@ruudboon.io>
2020-07-05 08:29:57 +0200

    Set correct repo

--- app/vue.config.js
+++ app/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
           {
             provider: "github",
             owner: "ruudboon",
-            repo: "ruudboon",
+            repo: "tally",
             releaseType: "draft",
             channel: "edge",
             url: "tally.ruudboon.io"
